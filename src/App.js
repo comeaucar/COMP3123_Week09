b87eda0b2d21bf735f7780b09788ae5e098a3302
comeaucar <cartercomeau@icloud.com>
2021-11-03 19:49:46 -0400

    Created new components and updated CSS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import College from "./College"
+import User from "./User"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="shadow-css">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{color:'black'}}>
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <User studentId="101253879" firstName="Carter" lastName="Comeau"/>
+        <College name="George Brown College, Toronto"/>
+        </div>
+        </div>
       </header>
     </div>
   );
